f096bed3528b0a13d3df51507d468366ce91c58f
Daniel Vega <vegadm57@gmail.com>
2022-05-06 17:09:10 -0300

    Desafio 2: Uso de funciones

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function limpiar(){
+    document.getElementById('miFormulario').reset();
+}
+function sumar(){
+    let x= parseInt(document.getElementById('valor1').value);
+    let y= parseInt(document.getElementById('valor2').value);
+    
+    document.getElementById('el-resultado').innerHTML=x+y;
+}
+function restar(){
+    let x= parseInt(document.getElementById('valor1').value);
+    let y= parseInt(document.getElementById('valor2').value);
+    
+    document.getElementById('el-resultado').innerHTML=x-y;
+}
+function multiplicar(){
+    let x= parseInt(document.getElementById('valor1').value);
+    let y= parseInt(document.getElementById('valor2').value);
+    
+    document.getElementById('el-resultado').innerHTML=x*y;
+}
+function dividir(){
+    let x= parseInt(document.getElementById('valor1').value);
+    let y= parseInt(document.getElementById('valor2').value);
+    
+    document.getElementById('el-resultado').innerHTML=x/y;
+}
+function calcularPorcentaje (numero, porcentaje){
+    return numero / 100* porcentaje;
+
+}
